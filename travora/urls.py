646a45c8848b68dcb34ca7ba8322592990ca612b
Dhita Irma <dhita.irma@gmail.com>
2020-10-10 01:14:55 +0800

    Create Login and Logout

--- travora/urls.py
+++ travora/urls.py
@@ -20,5 +20,9 @@ from users import views as user_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('login/', user_views.login_view, name='login'),
+    path('logout/', user_views.logout_view, name='logout'),
+
+
     path('', include('forum.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 from .forms import UserRegisterForm
@@ -19,3 +20,28 @@ def register(request):
         # Create an instance of an empty form
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+def login_view(request):
+    if request.method == "POST":
+
+        # Attempt to sign user in
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        # Check if authentication successful
+        if user is not None:
+            login(request, user)
+            return redirect(reverse("index"))
+        else:
+            return render(request, "forum/login.html", {
+                "message": "Invalid username and/or password."
+            })
+    else:
+        return render(request, "forum/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("index"))
