4d9e2c3413d6e7a1783fc062aba904784c52bda9
Dhita Irma <dhita.irma@gmail.com>
2020-10-10 01:02:53 +0800

    Create Register form

---
+++ users/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.urls import reverse
+
+from .forms import UserRegisterForm
+
+
+def register(request):
+    if request.method == 'POST':
+        # Create an instance and populate it with form data
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect(reverse('index'))
+
+    else:
+        # Create an instance of an empty form
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
