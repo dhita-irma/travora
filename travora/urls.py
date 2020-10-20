f6dc649f8abaa755665885b410b3ec58a97dd039
Dhita Irma <dhita.irma@gmail.com>
2020-10-20 19:14:31 +0800

    Revert "Set profile picture"
    This reverts commit 0f3ebe1f66e36fef71082dd713880f57dfc65160.

--- travora/urls.py
+++ travora/urls.py
@@ -17,20 +17,12 @@ from django.contrib import admin
 from django.urls import path, include
 from users import views as user_views
 
-# For serving static files during development
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
     path('login/', user_views.login_view, name='login'),
     path('logout/', user_views.logout_view, name='logout'),
-    path('profile/', user_views.profile, name='profile'),
+
 
     path('', include('forum.urls')),
 ]
-
-
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -14,7 +14,6 @@ def register(request):
             form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f'Account created for {username}!')
-            print('User Created')
             return redirect(reverse('index'))
 
     else:
@@ -46,7 +45,3 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect(reverse("index"))
-
-
-def profile(request):
-    return render(request, 'users/profile.html')
